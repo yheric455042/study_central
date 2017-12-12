febd850f1875171e5a06f04570e034ab5d84a670
yheric455042 <yheric455042@gmail.com>
2017-12-13 03:13:17 +0800

    modified Second 12/13

--- js/index.js
+++ js/index.js
@@ -1,21 +1,32 @@
 (function($) {
-  var page = ['about_company', 'about_service', 'about_team', 'school_australia', 'school_philippines', 'visa_australia', 'visa_philippines', 'student_benefit', 'lecture', '#student_sharing', 'contact_taiwan', 'contact_australia'];
+  var page = ['about_company', 'about_service', 'about_team', 'school_australia', 'school_philippines', 'visa_australia', 'visa_philippines', 'student_benefit', 'lecture', 'student_sharing', 'contact_taiwan', 'contact_australia'];
 
   $(window).on('hashchange',function() {
     var hash = window.location.hash ? window.location.hash.substring(1) : false;
-    var id = $('main section').attr('id');
     
+    $('section > div:visible').hide();
+
     if(hash && page.indexOf(hash) != -1) {
-      $('main section div').load('template/about_company.html');
+      // will use has varible
+      $('#'+'about_company').show();
     } else {
-      $('main section div').load('template/home_page.html');
+      $('#index').show();
     }
 
      
     
     }); 
   $(document).ready(function() {
-    $('main section div').load('template/home_page.html');
+    /*
+     * will use foreach to all html hide.
+     * */
+    
+    $.get('template/'+'about_company.html', function(data) {
+      var $element = $(data);
+      
+      $('main #index').after($element);
+      $element.hide();
+    });
     
   });
 
